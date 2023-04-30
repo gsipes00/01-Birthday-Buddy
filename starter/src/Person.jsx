9a9fdfa2f9ad942a8738c92fbc2e8309d76adb93
Gene Sipes <gsipes00@yahoo.com>
2023-04-30 14:51:44 -0500

    changed div element on person component jsx to an article

--- starter/src/Person.jsx
+++ starter/src/Person.jsx
@@ -1,13 +1,13 @@
 const Person = ({ name, age, image }) => {
   return (
-    <div>
+    <article className='person'>
       <h3>{name}</h3>
       <p>{age}</p>
       <img
         src={image}
         style={{ width: "200px", height: "200px", objectFit: "cover" }}
       />
-    </div>
+    </article>
   );
 };
 
