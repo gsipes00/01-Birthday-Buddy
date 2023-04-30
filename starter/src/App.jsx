c9205788028cb2ee28fa2a8219e869fe31c7e1a8
Gene Sipes <gsipes00@yahoo.com>
2023-04-30 14:16:57 -0500

    set up initial list element to map over people object

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -1,4 +1,25 @@
+import { useState } from "react";
+import data from "./data";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter test</h2>;
+  const [people, setPeople] = useState(data);
+  console.log(people);
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <div key={id}>
+            <h3>{name}</h3>
+            <p>{age}</p>
+            <img
+              src={image}
+              style={{ width: "200px", height: "200px", objectFit: "cover" }}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 export default App;
